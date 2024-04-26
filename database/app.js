77c354b4e682676ca35402c4736d26e289edaa2e
watanabe ryoma <watawata.ryo1231@gmail.com>
2024-04-26 18:15:33 +0900

    add code to connect and insert record

--- database/app.js
+++ database/app.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
+    port: '3306',
     user: 'root',
-    password: 'mysql',
-    database: 'test_db'
+    password: 'lOjit212',
+    database: 'test_db',
+    table: 'test_log'
 });
 
 // 接続
