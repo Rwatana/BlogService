db249adfa32ecc8e3b4cd449464094bd50db7a13
Rwatana <watawata.ryo1231@gmail.com>
2024-05-06 22:31:17 +0900

    add logging file/function

--- comments/index.js
+++ comments/index.js
@@ -3,7 +3,9 @@ const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
 const axios = require("axios");
-
+const mysql = require("mysql");
+const dbcn = require("../log/dbConnect");
+const { insertLog } = require("../log/dbSendLog");
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -38,11 +40,14 @@ app.post("/posts/:id/comments", async (req, res) => {
 });
 
 app.post("/events", async (req, res) => {
+  current_date = new Date();
+
   console.log("Event Received:", req.body.type);
 
   const { type, data } = req.body;
 
   if (type === "CommentModerated") {
+    insertLog(dbcn, current_date, req.body.type, 'demo');
     const { postId, id, status, content } = data;
     const comments = commentsByPostId[postId];
 
